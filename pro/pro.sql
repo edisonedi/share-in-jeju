3d061cc3d63d86b1bf794798efac23575231f8bc
moongjin97 <84491733+moongjin97@users.noreply.github.com>
2021-07-19 15:52:38 +0900

    Delete pro directory

--- pro/pro.sql
+++
@@ -1,9 +0,0 @@
-create table pmember (
-	id varchar2(20) primary key,
-	email varchar2(30),
-	password varchar2(30),
-	name varchar2(30),
-	del char(1) default 'n',
-	regdate date
-);
-select * from pmember;
